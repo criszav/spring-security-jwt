7f4f509595aae939f82ae92df994fd9fcd81c8c3
criszav <czavala.dev@gmail.com>
2023-09-26 10:14:38 -0300

    add login, register requests and token response

---
+++ src/main/java/com/czavala/springjwt/auth/AuthResponse.java
@@ -0,0 +1,15 @@
+package com.czavala.springjwt.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+
+    private String token;
+}
---
+++ src/main/java/com/czavala/springjwt/auth/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.czavala.springjwt.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+
+    private String username;
+    private String password;
+}
